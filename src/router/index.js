bccf396e490d09e6361e6a021c9125b9ceebfb26
cchura94 <cchura.cpc@gmail.com>
2022-10-10 22:30:17 -0400

    template Primevue

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import App from '../App.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../pages/LandingDemo.vue')
     },
     {
       path: '/about',
@@ -16,7 +16,40 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: App,
+      children: [
+        {
+            path: '',
+            name: 'dashboard',
+            component: () => import('../components/Dashboard.vue')
+        },
+        
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/Login.vue')
+  },
+  {
+    path: '/error',
+    name: 'error',
+    component: () => import('../pages/Error.vue')
+},
+{
+    path: '/notfound',
+    name: 'notfound',
+    component: () => import('../pages/NotFound.vue')
+},
+{
+    path: '/access',
+    name: 'access',
+    component: () => import('../pages/Access.vue')
+}
   ]
 })
 
